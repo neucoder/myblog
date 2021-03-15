32278090c8a6cd1ef95407893cad77a20c843784
ys <neucoder@163.com>
2021-03-15 17:10:32 +0800

    标签展示

--- src/main/java/com/ys/blog/service/BlogServiceImpl.java
+++ src/main/java/com/ys/blog/service/BlogServiceImpl.java
@@ -17,10 +17,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -40,6 +37,7 @@ public class BlogServiceImpl implements BlogService{
         return blogRepository.getOne(id);
     }
 
+    @Transactional
     @Override
     public Blog getAndConvert(Long id) {
          Blog blog = blogRepository.getOne(id);
@@ -51,7 +49,7 @@ public class BlogServiceImpl implements BlogService{
         BeanUtils.copyProperties(blog, b);
         String content = b.getContent();
         b.setContent(MarkdownUtils.markdownToHtmlExtensions(content));
-//        blogRepository.updateViews(id);
+        blogRepository.updateViews(id);
         return b;
     }
 
@@ -91,7 +89,13 @@ public class BlogServiceImpl implements BlogService{
 
     @Override
     public Page<Blog> listBlog(Long tagId, Pageable pageable) {
-        return null;
+        return blogRepository.findAll(new Specification<Blog>() {
+            @Override
+            public Predicate toPredicate(Root<Blog> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
+                Join join = root.join("tags");
+                return cb.equal(join.get("id"), tagId);
+            }
+        }, pageable);
     }
 
     @Override
